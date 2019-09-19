2177400c3fc7079e5ebfdf187cdbd953a0688787
sejung-park <withappy0127@gmail.com>
2019-09-19 21:37:00 +0900

    step 1-last : reset all test 1. npm init 2. npm install webpack webpack-cli --save-dev 3. add script(dev-build,build) in package.json and webpack.config.js 4. add index.html, index.js 5. npm run dev-build, check dist' response file 6. view index.html ( search local file'path in chrome browser)

---
+++ app/webpack.config.js
@@ -0,0 +1,20 @@
+var path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename:'bundle.js', // 없어도 됨 기본이름은 main.js 임
+        //path:path.resolve(__dirname, 'dist') // 없어도 됨, dist 경로가 기본 설정으로 됨
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+        //   use: {
+        //     loader: "babel-loader"
+        //   }
+        }
+      ]
+    }
+  };
